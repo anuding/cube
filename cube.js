e2b591a29a2f35a42e606ae556eb967d719f98ef
anuding <anuding@126.com>
2019-11-30 16:38:28 +0800

    able to rotate

--- cube.js
+++ cube.js
@@ -3,19 +3,19 @@ var camera;
 var renderer;
 var controls;
 var cubes = [];
+var transparentCube;
 var raycaster;
-var mouse;
 var colors = []
+
+
 function initApp() {
     raycaster = new THREE.Raycaster();
-    mouse = new THREE.Vector2();
-    mouse.x = -1;
-    mouse.y = 1;
+
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xbababa);
 
     var axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
+    // scene.add(axesHelper);
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 10;
@@ -24,14 +24,18 @@ function initApp() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
-
+    // controls.mouseButtons = {
+    //     LEFT: THREE.MOUSE.ROTATE,
+    //     MIDDLE: THREE.MOUSE.DOLLY,
+    //     RIGHT: THREE.MOUSE.PAN
+    // }
     document.body.appendChild(renderer.domElement);
-    document.addEventListener("keydown", onKeyDown, false);
     document.addEventListener("mousedown", onMouseDown, false);
     document.addEventListener("mouseup", onMouseUp, false);
     document.addEventListener("mousemove", onMouseMove, false);
 
 }
+
 function shading() {
     cubes.forEach(f => {
         f.geometry.colorsNeedUpdate = true;
@@ -108,29 +112,22 @@ function initScene() {
     }
     colors = [Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff, Math.random() * 0xffffff];
     shading()
+
+
+    var g = new THREE.BoxGeometry(3, 3, 3);
+    var m = new THREE.MeshBasicMaterial({ color: 0xffffff, opacity: 0.0 });
+    transparentCube = new THREE.Mesh(g, m);
+    transparentCube.material.transparent = true
+    scene.add(transparentCube);
 }
 
-function getFace(dir) {
+function getFace(face) {
     var plane;
-    switch (dir) {
-        case "top":
-            plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), -1);
+    for (var i = 0; i < planeNames.length; i++) {
+        if (face == planeNames[i]) {
+            plane = planes[i]
             break;
-        case "bottom":
-            plane = new THREE.Plane(new THREE.Vector3(0, -1, 0), -1);
-            break;
-        case "left":
-            plane = new THREE.Plane(new THREE.Vector3(-1, 0, 0), -1);
-            break;
-        case "right":
-            plane = new THREE.Plane(new THREE.Vector3(1, 0, 0), -1);
-            break;
-        case "front":
-            plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -1);
-            break;
-        case "back":
-            plane = new THREE.Plane(new THREE.Vector3(0, 0, -1), -1);
-            break;
+        }
     }
     var faces = []
     cubes.forEach(cube => {
@@ -146,92 +143,184 @@ function getFace(dir) {
 
 var pressed = false;
 var pivot = new THREE.Object3D()
-function doRotate(face, clockwise) {
+var planes = [
+    new THREE.Plane(new THREE.Vector3(0, 1, 0), -1),//top
+    new THREE.Plane(new THREE.Vector3(0, -1, 0), -1),//bottom
+    new THREE.Plane(new THREE.Vector3(-1, 0, 0), -1),//left
+    new THREE.Plane(new THREE.Vector3(1, 0, 0), -1),//right
+    new THREE.Plane(new THREE.Vector3(0, 0, 1), -1),//front
+    new THREE.Plane(new THREE.Vector3(0, 0, -1), -1),//back
+    new THREE.Plane(new THREE.Vector3(0, 1, 0), 0),//rowmid
+    new THREE.Plane(new THREE.Vector3(-1, 0, 0), 0),//colmid1
+    new THREE.Plane(new THREE.Vector3(0, 0, 1), 0)//colmid2
 
-    if (Math.abs(pivot.rotation.z) < Math.PI / 2) {
+]
+var planeNames = [
+    'top', 'bottom', 'left', 'right', 'front', 'back', 'rowmid', 'colmid1','colmid2'
+]
+function doRotate(face, axis, clockwise) {
+    moving = true;
+    if (Math.abs(pivot.rotation[axis]) < Math.PI / 2) {
         var faces = getFace(face)
         pivot.updateMatrixWorld();
         var active = []
         faces.forEach(f => { active.push(f) })
         active.forEach(f => { pivot.attach(f) })
-        pivot.rotateZ(clockwise * 0.1)
-        if (Math.abs(Math.abs(pivot.rotation.z) - Math.PI / 2) <= 0.2) {
-            pivot.rotateZ(clockwise * (Math.abs(Math.abs(pivot.rotation.z) - Math.PI / 2)))
+        pivot.rotation[axis] += (clockwise * 0.1)
+        if (Math.abs(Math.abs(pivot.rotation[axis]) - Math.PI / 2) <= 0.2) {
+            pivot.rotation[axis] += (clockwise * (Math.abs(Math.abs(pivot.rotation[axis]) - Math.PI / 2)))
         }
         console.log("rotating")
         pivot.updateMatrixWorld();
         active.forEach(f => { scene.attach(f) })
+        requestAnimationFrame(function () { doRotate(face, axis, clockwise) });
     }
     else {
         pressed = false;
+        moving = false;
     }
 
 }
 
 function render() {
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObjects(cubes);
-
-    if (intersects.length >= 1) {
-        var o = raycaster.intersectObject(intersects[0].object);
-        if (o.length >= 1) {
-            var i1 = 0, i2 = 0;
-
-            if (o[0].faceIndex % 2 == 0) {
-                i1 = o[0].faceIndex;
-                i2 = o[0].faceIndex + 1;
-            }
-            else {
-                i1 = o[0].faceIndex;
-                i2 = o[0].faceIndex - 1;
-            }
-            var castedFace1 = o[0].object.geometry.faces[i1]
-            var castedFace2 = o[0].object.geometry.faces[i2]
-            o[0].object.geometry.colorsNeedUpdate = true;
-
-            castedFace1.color.setRGB(1, 0, 0);
-            castedFace2.color.setRGB(1, 0, 0);
-            console.log(o[0])
-        }
-
-    }
-
-
-
-
-    if (pressed) {
-        doRotate("front", -1)
-    }
     controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
-function onMouseDown(event) { }
-function onMouseUp(event) { }
-function onMouseMove(event) {
+
+
+
+
+function getIntersect(event) {
+    var mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    // console.log(mouse.x, mouse.y)
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersectCube = null, normal = null;
+
+    var intersects = raycaster.intersectObjects(cubes);
+    if (intersects.length >= 1) {
+        intersectCube = intersects[0];
+    }
+    var intersects = raycaster.intersectObjects([transparentCube]);
+    if (intersects.length >= 1) {
+        normal = intersects[0].face.normal;
+    }
+    return { intersect: intersectCube, normal: normal }
 }
 
-function onKeyDown(event) {
-    var keycode = event.which;
-    if (pressed)
-        return;
-    if (keycode == 87) {
-        console.log("W")
-        pivot.rotation.set(0, 0, 0)
+var sPos = new THREE.Vector3();
+var moving = false;
+var sIntersect;
+var sIntersectNormal;
+
+function onMouseDown(event) {
+    if (moving)
+        return
+    var intersectObj = getIntersect(event)
+    if (intersectObj.intersect) {
+        sIntersect = intersectObj.intersect
+        sIntersectNormal = intersectObj.normal
+        sPos = sIntersect.point
         pressed = true;
+        controls.enabled = false;
     }
-    else if (keycode == 83) {
-        console.log("S")
-        pivot.rotation.set(0, 0, 0)
-        pressed = true;
-    }
-    else if (keycode == 32) {
-        console.log("Blank")
+}
+function onMouseMove(event) {
+    if (moving)
+        return
+    if (pressed) {
+        var intersectObj = getIntersect(event)
+        if (intersectObj.intersect) {
+            var ePos = intersectObj.intersect.point
+            var dragVector = ePos.sub(sPos)
+            var rotateDir = getRotateDir(dragVector, sIntersectNormal)
+            pivot.rotation.set(0, 0, 0)
+            pressed = true;
+            var face, axis, dir;
+            //which face
+            sIntersect.object.geometry.computeBoundingBox();
+            var bbox = sIntersect.object.geometry.boundingBox.clone();
+            sIntersect.object.updateMatrixWorld(true);
+            bbox.applyMatrix4(sIntersect.object.matrixWorld);
+            var dragDir = new THREE.Vector3(rotateDir.dragDir.axis == 'x' ? 1 : 0, rotateDir.dragDir.axis == 'y' ? 1 : 0, rotateDir.dragDir.axis == 'z' ? 1 : 0)
+            for (var i = 0; i < planes.length; i++) {
+                if (planes[i].normal.dot(sIntersectNormal) == 0 &&
+                    planes[i].normal.dot(dragDir) == 0 &&
+                    planes[i].intersectsBox(bbox)) {
+                    
+                    face = planeNames[i];
+                    break;
+                }
+            }
+            //which axis
+            if (rotateDir.dragDir.axis == rotateDir.normalDir.axis)
+                return
+            if (rotateDir.dragDir.axis == 'x' && rotateDir.normalDir.axis == 'z')
+                axis = "y"
+            if (rotateDir.dragDir.axis == 'z' && rotateDir.normalDir.axis == 'x')
+                axis = "y"
+            if (rotateDir.dragDir.axis == 'x' && rotateDir.normalDir.axis == 'y')
+                axis = "z"
+            if (rotateDir.dragDir.axis == 'y' && rotateDir.normalDir.axis == 'x')
+                axis = "z"
+            if (rotateDir.dragDir.axis == 'y' && rotateDir.normalDir.axis == 'z')
+                axis = "x"
+            if (rotateDir.dragDir.axis == 'z' && rotateDir.normalDir.axis == 'y')
+                axis = "x"
+
+            //which dir
+            dir = rotateDir.dragDir.dir
+            if (!face || !axis || !dir) {
+                console.log("error")
+                console.log(face, axis, dir)
+
+                return
+            }
+            console.log(face, axis, dir)
+            doRotate(face, axis, dir)
+            // doRotate('top', 'y', 1)
+            // doRotate('front', 'z', 1)
+
+            // console.log(rotateDir)
+        }
     }
 
+}
+function onMouseUp(event) {
+    pressed = false;
+    controls.enabled = true;
+}
+
+function getMaxCompoent(vec3) {
+    var absDx = Math.abs(vec3.x);
+    var absDy = Math.abs(vec3.y);
+    var absDz = Math.abs(vec3.z);
+    var axis;
+    var dir;
+    switch (Math.max(absDx, absDy, absDz)) {
+        case absDx:
+            dir = vec3.x > 0 ? 1 : -1
+            axis = 'x';
+            break;
+        case absDy:
+            dir = vec3.y > 0 ? 1 : -1
+            axis = 'y';
+            break;
+        case absDz:
+            dir = vec3.z > 0 ? 1 : -1
+            axis = 'z';
+            break;
+    }
+    return { dir: dir, axis: axis }
+}
+
+function getRotateDir(dragVector, intersectNormal) {
+    var dragDir = getMaxCompoent(dragVector)
+    var normalDir = getMaxCompoent(intersectNormal)
+    return { dragDir: dragDir, normalDir: normalDir }
 }
 initApp();
 initScene();
