cd23c5b28a347bc851f7040705a418ddc46ec66e
anuding <anuding@126.com>
2019-12-05 00:03:31 +0800

    refact old cube code as a lib

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CubeSolverComponent } from './cube-solver/cube-solver.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cube_solver', component: CubeSolverComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CubeSolverComponent } from './cube-solver/cube-solver.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeSolverComponent
   ],
   imports: [
     BrowserModule,
