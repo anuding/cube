6a30daed71dad2f531348015edcb90c8677fdd40
anuding <anuding@126.com>
2019-12-07 19:32:00 +0800

    add cube 101

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CubeSolverComponent } from './cube-solver/cube-solver.component';
+import { Cube101Component } from './cube101/cube101.component';
 
 
 const routes: Routes = [
-  { path: 'cube_solver', component: CubeSolverComponent }
+  { path: 'cubesolver', component: CubeSolverComponent },
+  { path: 'cube101', component: Cube101Component }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CubeSolverComponent } from './cube-solver/cube-solver.component';
+import { Cube101Component } from './cube101/cube101.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CubeSolverComponent
+    CubeSolverComponent,
+    Cube101Component
   ],
   imports: [
     BrowserModule,
