bec721b83fc4bdb1552b9d0c2ecfc099c253e209
anuding <anuding@126.com>
2019-12-15 20:38:18 +0800

    add fill color module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CubeSolverComponent } from './cube-solver/cube-solver.component';
 import { Cube101Component } from './cube101/cube101.component';
-
+import { FillColorComponent } from './fill-color/fill-color.component';
+import { ColorPickerModule } from 'ngx-color-picker';
 @NgModule({
   declarations: [
     AppComponent,
     CubeSolverComponent,
-    Cube101Component
+    Cube101Component,
+    FillColorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
